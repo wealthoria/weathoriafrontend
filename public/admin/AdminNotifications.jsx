9109560dac1a47bdea2713d7bf8fae640ed59c6e
anjalisrinivas9353-eng <anjalisrinivas9353@gmail.com>
2026-09-03 11:35:54 +0530

    Admin notifications

--- public/admin/AdminNotifications.jsx
+++ public/admin/AdminNotifications.jsx
@@ -7,8 +7,8 @@ const {
 
 function AdminNotifications() {
 
-  const API_BASE_URL =
-    "https://webinar-registration-backend.onrender.com/api/members/notifications";
+ const API_BASE_URL =
+  "https://webinar-registration-backend.onrender.com";
 
   const [members, setMembers] =
     useState([]);

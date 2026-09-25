b55a9b00cd925c5f4c40500412056ec98ace61bf
anjalisrinivas9353-eng <anjalisrinivas9353@gmail.com>
2026-09-25 11:27:35 +0530

    course coming soon

--- src/members/Course.jsx
+++ src/members/Course.jsx
@@ -1,23 +1,36 @@
+import React from "react";
+
 const CourseVideos = () => {
   return (
     <section className="wd-course-coming-soon">
+
+      {/* Desktop */}
       <img
         src="/assets/course-desktop.png"
         alt="Wealthoria Courses Coming Soon"
         className="wd-course-image wd-course-desktop"
       />
 
+      {/* Tablet */}
       <img
         src="/assets/course-tablet.png"
         alt="Wealthoria Courses Coming Soon"
         className="wd-course-image wd-course-tablet"
       />
 
+      {/* Mobile */}
       <img
         src="/assets/course-mobile.png"
         alt="Wealthoria Courses Coming Soon"
         className="wd-course-image wd-course-mobile"
       />
+
+      <p className="wd-course-description">
+        Courses are charged per lesson, giving you the freedom to choose
+        exactly what you want to learn. Simply select and purchase the
+        lessons that are relevant to you.
+      </p>
+
     </section>
   );
 };
